2457f59f77e93c808107551e28f13c61a201019c
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2021-09-02 16:44:57 +0100

    Update useAgora.jsx

--- src/Hooks/useAgora.jsx
+++ src/Hooks/useAgora.jsx
@@ -7,7 +7,7 @@ import { LOGIN } from 'Routes/route';
 const appId = 'fe911605b2f6467faf4e846035abab80'
 
 //should have stored in env but no need really since its for test
-const temptoken = '006fe911605b2f6467faf4e846035abab80IAB822rVYs8CVnIqL917bEnyStkegC4+z+SHdBq+7DxjfGnabjEAAAAAEAACPcDIhaAwYQEAAQCFoDBh'
+const temptoken = '006fe911605b2f6467faf4e846035abab80IAAaCwlQtjgu5zeP01IzRTODpnhxUnFrQrrAt3fsImI41mnabjEAAAAAEADxGgd40UIyYQEAAQDQQjJh'
 const channel = 'Elliot'
 
 
